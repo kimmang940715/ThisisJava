0b697b03f6f19df4f3a1743779a78253d8815c90
KIMHYEOK_GRAM <kimmang940715@naver.com>
2026-09-14 18:19:45 +0900

    정수 리터럴

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
         import java.util.Arrays;
         import java.util.Scanner;
 
+        /*
+        정수 : byte, char, short, int, long
+        실수 : float, double
+        논리 : boolean
+
+        메모리 사용 크기 (단위 bit)
+        byte : 8
+        short : 16
+        int : 32
+        long : 64
+         */
+
+
         public class Main {
             public static void main(String[] args) {
                 int x = 3;
