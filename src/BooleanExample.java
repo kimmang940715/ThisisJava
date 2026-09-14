c1a499d1a56ba6006a70f8a964574aa99ee735c8
KIMHYEOK_GRAM <kimmang940715@naver.com>
2026-09-14 19:08:51 +0900

    Boolean 형식, String 형식, TextBlock

---
+++ src/BooleanExample.java
@@ -0,0 +1,17 @@
+public class BooleanExample {
+    static void main() {
+        boolean stop = true;
+
+        if(stop) {
+            System.out.println("중지");
+        }else {
+            System.out.println("시작");
+        }
+
+        int x = 10;
+        boolean result1 = (x == 20);
+        boolean result2 = (x != 20);
+        System.out.println("result1 : " + result1);
+        System.out.println("result2 : " + result2);
+    }
+}
---
+++ src/TextBlockExample.java
@@ -0,0 +1,27 @@
+public class TextBlockExample {
+    static void main() {
+        String str1 = "" +
+                "{\n" +
+                "\t\"id\":\"winter\",\n" +
+                "\t\"name\":\"눈송이\"\n" +
+                "}";
+
+        String str2 = """
+                {
+                    "id":"winter",
+                    "name":"눈송이"
+                }
+                """;
+
+        System.out.println(str1);
+        System.out.println("-------------------");
+        System.out.println(str2);
+        System.out.println("-------------------");
+        String str = """
+                나는 자바를 
+                학습힙니다.
+                나는 자바 고수가 될겁니다
+                """;
+        System.out.println(str);
+    }
+}
