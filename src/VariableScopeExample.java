2b2c4feb5c08b3593beeb2b1fa8ec5d4af928170
KIMHYEOK_GRAM <kimmang940715@naver.com>
2026-09-17 01:04:33 +0900

    scanner 까지

---
+++ src/VariableScopeExample.java
@@ -0,0 +1,9 @@
+public class VariableScopeExample {
+    static void main() {
+        int v1 = 15;
+        if (v1 > 10) {
+            int v2 = v1 - 10;
+        }
+        //int v3 = v1 + v2 + 5 ; // v2변수를 사용할 수 없기때문에 컴파일 에러 발생
+    }
+}
