e380cf3fa4238be01ad8f81d1a699e6407b17971
KIMHYEOK_GRAM <kimmang940715@naver.com>
2026-09-17 23:22:42 +0900

    연산자

---
+++ src/IncreaseDecreaseOperatorExample.java
@@ -0,0 +1,32 @@
+public class IncreaseDecreaseOperatorExample {
+    static void main() {
+        int x = 10;
+        int y = 10;
+        int z;
+
+        x++;
+        ++x;
+        System.out.println("x = " + x);
+
+        System.out.println("--------------------");
+        y--;
+        --y;
+        System.out.println("y = " + y);
+
+        System.out.println("--------------------");
+        z = x++;
+        System.out.println("z = " + z);
+        System.out.println("x = " + x);
+
+        System.out.println("--------------------");
+        z = ++x;
+        System.out.println("z = " + z);
+        System.out.println("x = " + x);
+
+        System.out.println("--------------------");
+        z = ++x + y++;
+        System.out.println("z = " + z);
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+    }
+}
---
+++ src/SignOperatorExample.java
@@ -0,0 +1,11 @@
+public class SignOperatorExample {
+    static void main() {
+        int x = -100;
+        x = -x;
+        System.out.println("x : " + x);
+
+        byte b = 100;
+        int y  = -b;
+        System.out.println("y : " + y);
+    }
+}
