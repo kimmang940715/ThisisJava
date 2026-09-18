b1819cda480f9ef0e81e04313557967b77943754
KIMHYEOK_GRAM <kimmang940715@naver.com>
2026-09-18 23:10:21 +0900

    문자열 비교 equals()

---
+++ src/AccuracyExample1.java
@@ -0,0 +1,10 @@
+public class AccuracyExample1 {
+    static void main() {
+        int apple = 1;
+        double piceUnit = 0.1;
+        int number = 7;
+
+        double result = apple - number * piceUnit;
+        System.out.println("사과 1개에서 남은 양 : " + result);
+    }
+}
---
+++ src/InfinityAndNaNCheckExample.java
@@ -0,0 +1,13 @@
+public class InfinityAndNaNCheckExample {
+    static void main() {
+        int x = 5;
+        double y = 0.0;
+        double z = x / y;
+
+        if (Double.isInfinite(z) || Double.isNaN(z)){
+            System.out.println("값 산출 불가");
+        }else {
+            System.out.println(z + 2);
+        }
+    }
+}
