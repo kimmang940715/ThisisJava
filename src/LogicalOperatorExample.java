cc8a37784a5add7e199c6037776bc6e01928a3fe
KIMHYEOK_GRAM <kimmang940715@naver.com>
2026-09-19 23:45:41 +0900

    조건문

---
+++ src/LogicalOperatorExample.java
@@ -0,0 +1,30 @@
+public class LogicalOperatorExample {
+    static void main() {
+        int charCode =  'A';
+
+        if ((65 <= charCode) & (charCode <= 90)) {
+            System.out.println("대문자");
+        }
+
+        if ((97 <= charCode) && (charCode <= 122)) {
+            System.out.println("소문자");
+        }
+
+        if ((48 <= charCode) && (charCode <= 57)) {
+            System.out.println("0 ~ 9 숫자");
+        }
+
+        int value = 6;
+
+        if((value % 2 == 0 ) | (value % 3 == 0)) {
+            System.out.println("2 또는 3배수");
+        }
+
+        boolean result = (value % 2 == 0) || (value % 3 == 0);
+
+            if (!result) {
+                System.out.println("2또는 3의배수가 아님");
+            }
+
+    }
+}
