1d3f2b25ee393672feee06b382dcbc7d514e697b
KIMHYEOK_GRAM <boradore9407151@gmail.com>
2026-09-20 23:46:58 +0900

    객체 참조

---
+++ src/CarExample.java
@@ -0,0 +1,9 @@
+public class CarExample {
+    static void main() {
+        Car myCar = new Car();
+
+        System.out.println("모델명 : " + myCar.model);
+        System.out.println("시동 여부 : " + myCar.start);
+        System.out.println("현재 속도 : " + myCar.speed);
+    }
+}
