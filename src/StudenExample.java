d72a067f26880129f878ee1fcce7113f371035d1
KIMHYEOK_GRAM <kimmang940715@naver.com>
2026-09-20 23:42:20 +0900

    객체 참조

---
+++ src/StudenExample.java
@@ -0,0 +1,9 @@
+public class StudenExample {
+    static void main() {
+        Student s1 = new Student();
+        System.out.println("s1 변수가 Student 객체를 참조");
+        
+        Student s2 = new Student();
+        System.out.println("s2 변수가 또다른 Student 객체 참조");
+    }
+}
