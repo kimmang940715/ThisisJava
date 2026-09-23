cabcdcb90f3844d32797542e195c2a9821b8ec53
KIMHYEOK_GRAM <boradore9407151@gmail.com>
2026-09-23 23:37:52 +0900

    객체 필드값 변경

--- src/Car.java
+++ src/Car.java
@@ -1,5 +1,7 @@
 public class Car {
-    String model;
-    boolean start;
+    String company = "현대 자동차";
+    String model = "그랜저";
+    String color = "검정";
+    int maxSpeed = 350;
     int speed;
 }
--- src/CarExample.java
+++ src/CarExample.java
@@ -2,8 +2,14 @@ public class CarExample {
     static void main() {
         Car myCar = new Car();
 
+        System.out.println("제작회사 : " + myCar.company);
         System.out.println("모델명 : " + myCar.model);
-        System.out.println("시동 여부 : " + myCar.start);
+        System.out.println("색깔 : " + myCar.color);
+        System.out.println("최고속도 : " + myCar.maxSpeed);
         System.out.println("현재 속도 : " + myCar.speed);
+
+        // CAR 객체의 필드값 변경
+        myCar.speed = 60;
+        System.out.println("수정된 속도 : " + myCar.speed);
     }
 }
