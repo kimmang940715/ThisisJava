4fcab7fb2a96e451df4c4083a021b420697b908b
KIMHYEOK_GRAM <boradore9407151@gmail.com>
2026-09-24 00:36:45 +0900

    생성자 오버로딩

--- src/Car.java
+++ src/Car.java
@@ -1,7 +1,20 @@
 public class Car {
-    String company = "현대 자동차";
-    String model = "그랜저";
-    String color = "검정";
-    int maxSpeed = 350;
-    int speed;
+    String company = "현대자동차";
+    String model;
+    String color;
+    int maxSpeed;
+
+    Car(String model) {
+        this(model, "은색", 250);
+    }
+
+    Car(String model, String color) {
+        this(model, color, 250);
+    }
+
+    Car(String model, String color, int maxSpeed) {
+        this.model = model;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+    }
 }
--- src/CarExample.java
+++ src/CarExample.java
@@ -1,15 +1,19 @@
 public class CarExample {
     static void main() {
-        Car myCar = new Car();
+        Car car1 = new Car("자가용");
+        System.out.println("car1.company : " + car1.company);
+        System.out.println("car1.model : " + car1.model);
+        System.out.println();
 
-        System.out.println("제작회사 : " + myCar.company);
-        System.out.println("모델명 : " + myCar.model);
-        System.out.println("색깔 : " + myCar.color);
-        System.out.println("최고속도 : " + myCar.maxSpeed);
-        System.out.println("현재 속도 : " + myCar.speed);
+        Car car2 = new Car("자가용", "빨강");
+        System.out.println("car2.company : " + car2.company);
+        System.out.println("car2.model : " + car2.model);
+        System.out.println("car2.color : " + car2.color);
 
-        // CAR 객체의 필드값 변경
-        myCar.speed = 60;
-        System.out.println("수정된 속도 : " + myCar.speed);
+        Car car3 = new Car("택시", "검정", 200);
+        System.out.println("car3.company : " + car3.company);
+        System.out.println("car3.model : " + car3.model);
+        System.out.println("car3.color : " + car3.color);
+        System.out.println("car3.maxSpeed :" + car3.maxSpeed);
     }
 }
