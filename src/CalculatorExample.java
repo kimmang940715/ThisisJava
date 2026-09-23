07f91bd80eed5be766b7eaf4d31dbd8e523c6bd8
KIMHYEOK_GRAM <boradore9407151@gmail.com>
2026-09-24 00:54:12 +0900

    가변길이 매개변수 메소드

---
+++ src/CalculatorExample.java
@@ -0,0 +1,18 @@
+public class CalculatorExample {
+    static void main() {
+        Calculator myCalc = new Calculator();
+
+        myCalc.powerOn();
+
+        int result1 = myCalc.plus(5,6);
+        System.out.println("result1 : " + result1);
+
+        int x  = 10;
+        int y = 4;
+
+        double result2 = myCalc.divide(x,y);
+        System.out.println("result2 : " + result2);
+
+        myCalc.powerOff();;
+    }
+}
