1ea9f08168b1d3fb24c9ca3f19a83402a9946736
KIMHYEOK_GRAM <boradore9407151@gmail.com>
2026-09-25 23:45:33 +0900

    패키지 사용

---
+++ src/hyundai/Car.java
@@ -0,0 +1,12 @@
+package hyundai;
+
+import kumho.AllSeasonTire;
+import hankook.SnowTire;
+
+
+public class Car {
+    hankook.Tire tire1 = new hankook.Tire();
+    kumho.Tire tire2 = new kumho.Tire();
+    SnowTire tire3 = new SnowTire();
+    AllSeasonTire tire4 = new AllSeasonTire();
+}
