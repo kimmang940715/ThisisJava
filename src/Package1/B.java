517d5cdf74a16a1eb44543384909948a3b3647e3
KIMHYEOK_GRAM <boradore9407151@gmail.com>
2026-09-26 00:50:03 +0900

    접근 제한자

---
+++ src/Package1/B.java
@@ -0,0 +1,8 @@
+package Package1;
+
+public class B {
+    A a1 = new A(true);
+    A a2 = new A(1);
+    // A a3 = new A("문자열"); private 생성자 접근 불가
+
+}
---
+++ src/Package2/C.java
@@ -0,0 +1,10 @@
+package Package2;
+
+import Package1.*;
+
+public class C {
+    
+    A a1 = new A(true);
+   // A a2 = new A(1); //default 생성자로 접근 불가
+   // A a3 = new A("문자열"); // private 생성자로 접근 불가
+}
