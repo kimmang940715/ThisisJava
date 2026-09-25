66e348fa2ac078cad01f94d5c7011fc4c1858a53
KIMHYEOK_GRAM <boradore9407151@gmail.com>
2026-09-25 23:38:54 +0900

    final 키워드

--- src/Korean.java
+++ src/Korean.java
@@ -1,11 +1,12 @@
 public class Korean {
     String nation = "대한민국";
-    String name;
     String ssn;
 
+    String name;
+
     // 생성자 선언
-    public Korean(String n, String s) {
-        name = n;
-        ssn = s;
+    public Korean(String ssn, String name) {
+        this.ssn = ssn;
+        this.name = name;
     }
 }
