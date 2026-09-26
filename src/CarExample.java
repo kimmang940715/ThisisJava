1588bff27b5fe09480600b6be534d6e09cadca15
KIMHYEOK_GRAM <boradore9407151@gmail.com>
2026-09-27 00:53:24 +0900

    getter / setter

--- src/CarExample.java
+++ src/CarExample.java
@@ -1,10 +1,20 @@
 public class CarExample {
     static void main() {
-        Car myCar = new Car("포르쉐");
-        Car yourCar = new Car("벤츠");
 
-        myCar.run();
-        yourCar.run();
+        Car myCar = new Car();
+
+        myCar.setSpeed(-50);
+        System.out.println("현재 속도 : " + myCar.getSpeed());
+
+
+        myCar.setSpeed(50);
+        System.out.println("현재 속도 : " + myCar.getSpeed());
+
+        if (!myCar.isStop()) {
+            myCar.setStop(true);
+        }
+        System.out.println("현재 속도 : " + myCar.getSpeed());
+
     }
     
     
